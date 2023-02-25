4271e726aa414ec55178905400d84f1917320171
Maks Semenko <max_semenko@ukr.net>
2023-02-25 17:13:05 +0200

    lesson 42

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello from Notes app.")
+    template = loader.get_template('notes/index.html')
+    return HttpResponse(template.render())
